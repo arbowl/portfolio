7c8d22d4cb23a1be979ac0e1f04dd6f8a4c00a1d
arbowl <arbowl@tutanota.com>
2022-04-06 13:40:08 -0400

    Start creation and assimilation of Rest API

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+import os
 
 # Create your models here.
+
+def pull_server_data(server):
+    if server.lower() == 'valheim':
+        game = server
+        directory = '~/.config/unity3d/IronGate/Valheim/worlds'
+    if server.lower == 'minecraft':
+        game = server
+        directory = ''
+    
+    date = os.path.getmtime(directory)
+    file_size = os.path.getsize(directory)
+
+def pull_music_bot():
+    directory = '/home/drew/Documents/MusicBot'
+    date = os.path.getmtime(directory)
+    version = ''
+    server = ''
+
+class GameServer(models.Model):
+    game = models.CharField(max_length=100, default="", unique=False)
+    date = models.DateTimeField(auto_now_add=False)
+    directory = models.CharField(max_length=100, default="", unique=False)
+    file_size = models.IntegerField(null=False, default=0, unique=True)
+
+class MusicBot(models.Model):
+    date = models.DateTimeField(auto_now_add=False)
+    directory = models.CharField(max_length=100, default="/home/drew/Documents/MusicBot", unique=False)
+    version = models.CharField(max_length=100, default="", unique=False)
+    server = models.CharField(max_length=100, default="", unique=False)
--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from .views import GameView, MusicView
 
 urlpatterns = [
     path('', views.index),
     path('about.html', views.about),
     path('contact.html', views.contact),
+    path('servers/valheim', GameView.as_view()),
+    path('servers/valheim', GameView.as_view()),
+    path('servers/musicbot', MusicView.as_view()),
 ]
