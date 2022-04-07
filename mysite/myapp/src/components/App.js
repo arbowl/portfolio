cdc7b1b02ed737875b5e5fd8e55078b345990aae
arbowl <arbowl@tutanota.com>
2022-04-07 15:47:59 -0400

    Change spacing of main elements

--- mysite/myapp/src/components/App.js
+++ mysite/myapp/src/components/App.js
@@ -13,5 +13,28 @@ export default class App extends Component {
     }
 }
 
+export class Updates extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>News &amp; Updates</h1>
+                <hr></hr>
+                    <p>Lorem ipusm blah blah blah</p>
+                    <p>Another lorem ipsum</p>
+                <hr></hr>
+                <p>Stuff goes here</p>
+                <p>Maybe someday... but not today. Today just has filler garbage to take up space. Like this!</p>
+            </div>
+        );
+    }
+}
+
 const appDiv = document.getElementById("app");
 render(<App />, appDiv);
+
+const updatesDiv = document.getElementById("updates");
+render(<Updates />, updatesDiv)
