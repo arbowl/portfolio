5b2f98415fd5b3e637dda06a1517c99222002fae
arbowl <arbowl@tutanota.com>
2022-04-13 12:51:24 -0400

    Add content to sidebar pages

--- mysite/myapp/src/components/Album.js
+++ mysite/myapp/src/components/Album.js
@@ -16,6 +16,7 @@ import ImageListItemBar from '@mui/material/ImageListItemBar';
 import ListSubheader from '@mui/material/ListSubheader';
 import IconButton from '@mui/material/IconButton';
 import InfoIcon from '@mui/icons-material/Info';
+import img01 from "../../static/frontend/drewsophiegrad.png";
 
 export default class Album extends Component {
     constructor(props) {
@@ -37,7 +38,7 @@ export default class Album extends Component {
                     />
                     <ImageListItemBar
                         title={item.title}
-                        subtitle={item.author}
+                        subtitle={item.caption}
                         actionIcon={
                         <IconButton
                             sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
@@ -58,9 +59,9 @@ export default class Album extends Component {
 
 const itemData = [
     {
-      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-      title: 'Breakfast',
-      author: '@bkristastucchio',
+      img: img01,
+      title: 'Graduation',
+      caption: 'Me and Sophia (May 2021)',
       rows: 2,
       cols: 2,
       featured: true,
@@ -68,29 +69,29 @@ const itemData = [
     {
       img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
       title: 'Burger',
-      author: '@rollelflex_graphy726',
+      caption: '@rollelflex_graphy726',
     },
     {
       img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
       title: 'Camera',
-      author: '@helloimnik',
+      caption: '@helloimnik',
     },
     {
       img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
       title: 'Coffee',
-      author: '@nolanissac',
+      caption: '@nolanissac',
       cols: 2,
     },
     {
       img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
       title: 'Hats',
-      author: '@hjrc33',
+      caption: '@hjrc33',
       cols: 2,
     },
     {
       img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
       title: 'Honey',
-      author: '@arwinneil',
+      caption: '@arwinneil',
       rows: 2,
       cols: 2,
       featured: true,
@@ -98,34 +99,34 @@ const itemData = [
     {
       img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
       title: 'Basketball',
-      author: '@tjdragotta',
+      caption: '@tjdragotta',
     },
     {
       img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
       title: 'Fern',
-      author: '@katie_wasserman',
+      caption: '@katie_wasserman',
     },
     {
       img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
       title: 'Mushrooms',
-      author: '@silverdalex',
+      caption: '@silverdalex',
       rows: 2,
       cols: 2,
     },
     {
       img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
       title: 'Tomato basil',
-      author: '@shelleypauls',
+      caption: '@shelleypauls',
     },
     {
       img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
       title: 'Sea star',
-      author: '@peterlaster',
+      caption: '@peterlaster',
     },
     {
       img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
       title: 'Bike',
-      author: '@southside_customs',
+      caption: '@southside_customs',
       cols: 2,
     },
   ];
